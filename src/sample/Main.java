e40a83891be3004ecc90eff1afa6caca9a799e15
Artem <artem@gmail.com>
2019-08-02 22:09:28 +0300

    Реалізовано головне меню гри

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -25,16 +25,89 @@ public class Main extends Application {
     RadioButton rdButComplexityEasy;
     RadioButton rdButComplexityHard;
 
+    MenuInfo menuInfo;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         startInitialSettings();
         setPosition();
 
+        checkEventMenu();
+        if (checkDataMenu())
+            createGame();
+
 
         primaryStage.setTitle("Гра слова");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public boolean checkDataMenu()
+    {
+        return true;
+    }
+
+    public void createGame()
+    {
+        System.out.println("creating game...");
+    }
+
+
+    public void checkEventMenu()
+    {
+        rdButHuman.setOnAction(e -> {
+            rdButHuman.setSelected(true);
+            rdButComputer.setSelected(false);
+        });
+
+        rdButComputer.setOnAction(e -> {
+            rdButHuman.setSelected(false);
+            rdButComputer.setSelected(true);
+        });
+
+        rdButComplexityEasy.setOnAction(e -> {
+            rdButComplexityEasy.setSelected(true);
+            rdButComplexityHard.setSelected(false);
+        });
+
+        rdButComplexityHard.setOnAction(e -> {
+            rdButComplexityEasy.setSelected(false);
+            rdButComplexityHard.setSelected(true);
+        });
+
+        startButton.setOnAction(e -> {
+            String player,complexity;
+
+            if (rdButHuman.isSelected())
+                player = "Людина";
+            else
+                player = "Комп'ютер";
+
+            if (rdButComplexityEasy.isSelected())
+                complexity = "Легко";
+            else
+                complexity = "Важко";
+            menuInfo = new MenuInfo(spinnerSizeMatrixWord.getValue(),
+                    spinnerMaxNumberWordsInMatrix.getValue(),player,complexity);
+            setVisibleMenu(false);
+        });
+
+    }
+
+    public void setVisibleMenu(boolean flag)
+    {
+        startButton.setVisible(flag);
+        labelNameGame.setVisible(flag);
+        labelSizeMatrixWord.setVisible(flag);
+        labelMaxNumberWordsInMatrix.setVisible(flag);
+        labelComplexity.setVisible(flag);
+        labelPlayer.setVisible(flag);
+        spinnerMaxNumberWordsInMatrix.setVisible(flag);
+        spinnerSizeMatrixWord.setVisible(flag);
+        rdButComputer.setVisible(flag);
+        rdButHuman.setVisible(flag);
+        rdButComplexityEasy.setVisible(flag);
+        rdButComplexityHard.setVisible(flag);
     }
 
     public void setPosition()
@@ -43,6 +116,8 @@ public class Main extends Application {
         labelNameGame.setLayoutY(200);
         Font font=new Font(50);
         labelNameGame.setFont(font);
+        rdButHuman.setLayoutY(200);
+        rdButComputer.setLayoutY(300);
     }
 
     public void startInitialSettings()
---
+++ src/sample/MenuInfo.java
@@ -0,0 +1,50 @@
+package sample;
+
+public class MenuInfo {
+    private Integer sizeMatrix;
+    private Integer numberWords;
+    private String player;
+    private String complexity;
+
+    public MenuInfo() {
+    }
+
+    public MenuInfo(int sizeMatrix, int numberWords, String player, String complexity) {
+        this.sizeMatrix = sizeMatrix;
+        this.numberWords = numberWords;
+        this.player = player;
+        this.complexity = complexity;
+    }
+
+    public int getSizeMatrix() {
+        return sizeMatrix;
+    }
+
+    public void setSizeMatrix(int sizeMatrix) {
+        this.sizeMatrix = sizeMatrix;
+    }
+
+    public int getNumberWords() {
+        return numberWords;
+    }
+
+    public void setNumberWords(int numberWords) {
+        this.numberWords = numberWords;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(String complexity) {
+        this.complexity = complexity;
+    }
+}
