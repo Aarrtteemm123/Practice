7ad8094bafb7095f6a439b777c2d28b263b551ce
Artem <artem@gmail.com>
2019-08-04 15:22:08 +0300

    Створено меню гри

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -36,6 +37,22 @@ public class Main extends Application {
 
     private Button[][] matrixWords;
     private Button butFinishGame;
+    private Button butFinishStep;
+    private TextArea txtArAllWords;
+    private TextArea txtArFirstPlayerWords;
+    private TextArea txtArSecondPlayerWords;
+    private Label labTimer;
+    private Label labTypeFirstPlayer;
+    private Label labTypeSecondPlayer;
+    private Label labScoreFirstPlayer;
+    private Label labScoreSecondPlayer;
+    private Label labTitle;
+    private Label labInfoSizeMatrix;
+    private Label labInfoNumberWords;
+    private Label labInfoComplexity;
+    private Line lnUp;
+    private Line lnDown;
+    private Line lnRight;
 
 //----------------------------------------------//
 
@@ -47,19 +64,20 @@ public class Main extends Application {
     private int minNumberWords = 1;
     private int maxNumberWords = 100;
     private int startValueWords = 5;
-    private int minSize = 3;
-    private int maxSize = 25;
+    private int minSize = 4;
+    private int maxSize = 20;
     private int startValueSize = 10;
 
 //----------------------------------------------//
 
     @Override
     public void start(Stage primaryStage) {
+        startInitialData();
         startInitialSettingsMenu();
         startInitialSettingsGame();
         setVisibleGame(false);
-        setPositionInMenu();
-        setPositionInGame();
+        setPositionElementInMenu();
+        setPositionElementInGame();
         checkEventMenu();
         checkEventGame();
 
@@ -73,6 +91,15 @@ public class Main extends Application {
         buildMatrixButton(menuInfo.getSizeMatrix(),menuInfo.getSizeMatrix());
         setVisibleMenu(false);
         setVisibleGame(true);
+        setInfoForGame();
+    }
+
+    public void setInfoForGame()
+    {
+        labInfoComplexity.setText("Складність : "+menuInfo.getComplexity());
+        labInfoNumberWords.setText("Слів : "+menuInfo.getNumberWords());
+        labInfoSizeMatrix.setText("Розмір поля : "+menuInfo.getSizeMatrix()+
+                "x"+menuInfo.getSizeMatrix());
     }
 
     public void buildMatrixButton(int length,int height)
@@ -86,8 +113,8 @@ public class Main extends Application {
                 root.getChildren().add(matrixWords[x][y]);
                 matrixWords[x][y].setMaxSize(40,40);
                 matrixWords[x][y].setMinSize(5,5);
-                matrixWords[x][y].setLayoutX(x*30+100);
-                matrixWords[x][y].setLayoutY(y*30+100);
+                matrixWords[x][y].setLayoutX(x*30+20);
+                matrixWords[x][y].setLayoutY(y*30+70);
 
                 int finalX = x;
                 int finalY = y;
@@ -145,14 +172,40 @@ public class Main extends Application {
     public void checkEventGame()
     {
         butFinishGame.setOnAction(e -> {
+            removeMatrixWords();
             setVisibleGame(false);
             setVisibleMenu(true);
         });
     }
 
+    public void removeMatrixWords()
+    {
+        for (int i = 0; i < matrixWords.length; i++)
+        {
+            for (int j = 0; j < matrixWords[i].length; j++)
+                root.getChildren().remove(matrixWords[i][j]);
+        }
+    }
+
     public void setVisibleGame(boolean flag)
     {
         butFinishGame.setVisible(flag);
+        butFinishStep.setVisible(flag);
+        lnUp.setVisible(flag);
+        lnRight.setVisible(flag);
+        lnDown.setVisible(flag);
+        txtArAllWords.setVisible(flag);
+        txtArFirstPlayerWords.setVisible(flag);
+        txtArSecondPlayerWords.setVisible(flag);
+        labTypeFirstPlayer.setVisible(flag);
+        labTypeSecondPlayer.setVisible(flag);
+        labScoreFirstPlayer.setVisible(flag);
+        labScoreSecondPlayer.setVisible(flag);
+        labTimer.setVisible(flag);
+        labTitle.setVisible(flag);
+        labInfoSizeMatrix.setVisible(flag);
+        labInfoNumberWords.setVisible(flag);
+        labInfoSizeMatrix.setVisible(flag);
     }
 
     public void setVisibleMenu(boolean flag)
@@ -171,8 +224,10 @@ public class Main extends Application {
         rdButComplexityHard.setVisible(flag);
     }
 
-    public void setPositionInMenu()
+    public void setPositionElementInMenu()
     {
+        butStart.setLayoutX(200);
+        butStart.setLayoutY(200);
         labNameGame.setLayoutX(500);
         labNameGame.setLayoutY(200);
         Font font=new Font(50);
@@ -185,24 +240,68 @@ public class Main extends Application {
         rdButComplexityHard.setLayoutY(800);
     }
 
-    public void setPositionInGame()
+    public void setPositionElementInGame()
     {
         butFinishGame.setLayoutX(200);
         butFinishGame.setLayoutY(700);
+        butFinishStep.setLayoutX(900);
+        txtArAllWords.setLayoutX(1000);
+        txtArSecondPlayerWords.setLayoutY(300);
+        txtArFirstPlayerWords.setLayoutY(500);
+        txtArFirstPlayerWords.setLayoutX(800);
     }
 
     public void startInitialSettingsGame()
     {
         butFinishGame = new Button("Завершити гру");
+        butFinishStep = new Button("Заершити хід");
+
+        txtArAllWords = new TextArea("All");
+        txtArFirstPlayerWords = new TextArea("First");
+        txtArSecondPlayerWords = new TextArea("Second");
+
+        lnDown = new Line(0,800,1400,800);
+        lnRight = new Line(900,0,900,1000);
+        lnUp = new Line(0,100,1400,100);
+
+        labInfoComplexity = new Label();
+        labInfoNumberWords = new Label();
+        labInfoSizeMatrix = new Label();
+        labTimer = new Label();
+        labTitle = new Label();
+        labScoreFirstPlayer = new Label();
+        labScoreSecondPlayer = new Label();
+        labTypeFirstPlayer = new Label();
+        labTypeSecondPlayer = new Label();
+
+        root.getChildren().add(labTypeFirstPlayer);
+        root.getChildren().add(labTypeSecondPlayer);
+        root.getChildren().add(labTimer);
+        root.getChildren().add(labTitle);
+        root.getChildren().add(labScoreFirstPlayer);
+        root.getChildren().add(labScoreSecondPlayer);
+        root.getChildren().add(labInfoSizeMatrix);
+        root.getChildren().add(labInfoNumberWords);
+        root.getChildren().add(labInfoComplexity);
+        root.getChildren().add(txtArAllWords);
+        root.getChildren().add(txtArFirstPlayerWords);
+        root.getChildren().add(txtArSecondPlayerWords);
+        root.getChildren().add(butFinishStep);
         root.getChildren().add(butFinishGame);
+        root.getChildren().add(lnUp);
+        root.getChildren().add(lnDown);
+        root.getChildren().add(lnRight);
     }
 
-    public void startInitialSettingsMenu()
+    public void startInitialData()
     {
         root = new Pane();
         scene = new Scene(root, widthWindow, heightWindow);
         menuInfo = new MenuInfo();
+    }
 
+    public void startInitialSettingsMenu()
+    {
         spinMaxNumberWordsInMatrix = new Spinner<>();
         SpinnerValueFactory<Integer> valueFactory =
                 new SpinnerValueFactory.IntegerSpinnerValueFactory
